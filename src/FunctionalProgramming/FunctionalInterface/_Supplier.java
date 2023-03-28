cde44def5634f763a319e1d8e3419fdf3c3778a5
minha <minhaz.ahmed@student.hs-rm.de>
2023-03-28 17:55:33 +0200

    supplier

--- src/FunctionalProgramming/FunctionalInterface/_Supplier.java
+++ src/FunctionalProgramming/FunctionalInterface/_Supplier.java
@@ -1,4 +1,25 @@
 package FunctionalProgramming.FunctionalInterface;
 
-public class _Supplier {
+import java.util.List;
+import java.util.function.Supplier;
+
+class _Supplier {
+
+  static String represent() {
+    return "(method) I am an represent";
+  }
+
+  static Supplier<String> representSupplier = () -> "I am an represent";
+
+  static Supplier<List<String>> listOfRepresentSupplier =
+          () -> List.of("I am an represent Number 1", "I am an represent Number 2");
+
+  public static void main(String[] args) {
+    System.out.println(represent());
+    System.out.println(representSupplier.get());
+    System.out.println();
+    System.out.println(listOfRepresentSupplier.get());
+    System.out.println(listOfRepresentSupplier.get().get(0));
+    System.out.println(listOfRepresentSupplier.get().get(1));
+  }
 }
