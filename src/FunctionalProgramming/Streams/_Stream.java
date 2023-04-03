6a28a06d00ba59ced893dd305912eb9a4739d454
minha <minhaz.ahmed@student.hs-rm.de>
2023-04-03 21:24:09 +0200

    Stream

--- src/FunctionalProgramming/Streams/_Stream.java
+++ src/FunctionalProgramming/Streams/_Stream.java
@@ -9,7 +9,7 @@ import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
-public class _Stream {
+class _Stream {
 
   public static void main(String[] args) {
     List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
